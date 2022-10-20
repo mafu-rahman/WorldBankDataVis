bca5cd23707efb622e3e6c32c94669d4e4b3313d
hasan-arham <77273843+hasan-arham@users.noreply.github.com>
2022-10-20 04:46:57 -0400

    updated test cases

--- WorldBankAPI/src/main/java/tests/TestCases.java
+++ WorldBankAPI/src/main/java/tests/TestCases.java
@@ -1,4 +1,5 @@
 package tests;
+import login.*;
 
 import static org.junit.Assert.*;
 
@@ -9,6 +10,29 @@ public class TestCases {
 	@Test
 	public void test() {
 		fail("Not yet implemented");
+	}
+	
+	// Test if system allows login when credentials are correct
+	@Test 
+	public void test_login_01() {
+	}
+	
+	// Test if system allows login when credentials are correct
+	@Test
+	public void test_login_02() {
+		
+	}
+	
+	// Test if system allows login when credentials are not provided
+	@Test
+	public void test_login_03() {
+		
+	}
+	
+	// Test if system allows login when username has an empty character at the end
+	@Test 
+	public void test_login_04() {
+		
 	}
 
 }
