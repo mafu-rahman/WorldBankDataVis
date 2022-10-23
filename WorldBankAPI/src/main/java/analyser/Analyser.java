425939c6f4482259ec0353ddfbc94349ced9b88f
hasan-arham <77273843+hasan-arham@users.noreply.github.com>
2022-10-23 19:04:43 -0400

    added javadoc to analyser

--- WorldBankAPI/src/main/java/analyser/Analyser.java
+++ WorldBankAPI/src/main/java/analyser/Analyser.java
@@ -4,10 +4,19 @@ public class Analyser {
 	
 	private double mean;
 	
+	/**
+	 * Constructor method
+	 */
 	public Analyser() {
 		this.mean = 0;
 	}
 	
+	/**
+	 * Method used to compute mean given two parameters
+	 * @param sum  the total value of all elements
+	 * @param totalnums total number of elements from which mean is to be derived
+	 * @return mean the mean value
+	 */
 	public double calcMean(double sum, double totalnums) {
 		if(sum == 0) {
 			throw new IllegalArgumentException("Error: Attemping to divide zero by a number!");
@@ -16,10 +25,17 @@ public class Analyser {
 		return this.mean;
 	}
 	
+	/**
+	 * Print the mean to console
+	 */
 	public void printMean() {
 		System.out.println("The mean is " + this.mean);
 	}
 	
+	/**
+	 * Getter Method
+	 * @return the mean
+	 */
 	public double getMean() {
 		return this.mean;
 	}
