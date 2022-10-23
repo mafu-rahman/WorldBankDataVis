fe77db0f667ff0f5db5924840be335a263cccde5
hasan-arham <77273843+hasan-arham@users.noreply.github.com>
2022-10-23 19:16:13 -0400

    added Javadoc documentation folder

---
+++ WorldBankAPI/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"analyser","l":"Analyser"},{"p":"gui","l":"DATAPARSER"},{"p":"dataFetchers","l":"Fetcher"},{"p":"login","l":"loginUI"},{"p":"gui","l":"mainUI"},{"p":"gui","l":"Visualization"}];updateSearchResults();
