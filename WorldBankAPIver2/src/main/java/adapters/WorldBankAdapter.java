cbb20e0907c1de504ac20a0f1c149f20d0565099
hasan-arham <77273843+hasan-arham@users.noreply.github.com>
2022-11-26 23:05:37 -0500

    added more javadoc

--- WorldBankAPIver2/src/main/java/adapters/WorldBankAdapter.java
+++ WorldBankAPIver2/src/main/java/adapters/WorldBankAdapter.java
@@ -14,6 +14,13 @@ public class WorldBankAdapter implements TargetAdapter{
 		fetcher = new worldBankFetcher();
 	}
 
+	/**
+	 * Fetch data based on user provided data selection and analysis type
+	 * and Implement fetching and converting XML to JSON format
+	 * @param selection data composed of country code, start year and end year 
+	 * @param analysisTypeCode type of analysis
+	 * @return Object data object returned using the fetchData method
+	 */
 	public JsonArray fetchData(UserSelection selection, String analysisTypeCode) {
 		
 		JsonArray j = (JsonArray) fetcher.fetchData(selection, analysisTypeCode);
