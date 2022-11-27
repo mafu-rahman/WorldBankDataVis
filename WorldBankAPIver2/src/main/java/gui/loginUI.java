eb3b3105b5ff5b2265ceb6325fe19f280aeb339b
hasan-arham <77273843+hasan-arham@users.noreply.github.com>
2022-11-26 22:52:53 -0500

    changed: loginUI, removed ununsed library

--- WorldBankAPIver2/src/main/java/gui/loginUI.java
+++ WorldBankAPIver2/src/main/java/gui/loginUI.java
@@ -12,7 +12,6 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
-import javax.swing.SwingConstants;
 
 public class loginUI extends JFrame{
 	
