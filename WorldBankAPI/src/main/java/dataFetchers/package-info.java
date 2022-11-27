16192a51060b50cbe7d6b36fe9bb7ae3acb03e66
hasan-arham <77273843+hasan-arham@users.noreply.github.com>
2022-11-26 20:22:40 -0500

    removed folder: Original WorldBankAPI

--- WorldBankAPI/src/main/java/dataFetchers/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This module is used to fetch data from the given World Bank API 
- * 
- * @since 1.0
- * @version 1.1
- */
-package dataFetchers;
--- WorldBankAPI/src/main/java/login/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * The login module is composed of the loginUI class which provides a 
- * user a login interface.
- * 
- * @since 1.0
- * @version 1.0
- */
-package login;
