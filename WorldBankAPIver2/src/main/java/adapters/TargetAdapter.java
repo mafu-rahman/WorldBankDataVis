e27a034aaae2181571dbe60f195ce5c383d4bf06
hasan-arham <77273843+hasan-arham@users.noreply.github.com>
2022-11-26 23:02:05 -0500

    added javadoc to adapters.TargetAdapter

--- WorldBankAPIver2/src/main/java/adapters/TargetAdapter.java
+++ WorldBankAPIver2/src/main/java/adapters/TargetAdapter.java
@@ -4,6 +4,12 @@ import client.UserSelection;
 
 public interface TargetAdapter {
 	
+	/**
+	 * Fetch data based on user provided data selection and analysis type
+	 * @param selection data composed of country code, start year and end year 
+	 * @param analysisTypeCode type of analysis
+	 * @return Object data object returned using the fetchData method
+	 */
 	public Object fetchData(UserSelection selection, String analysisTypeCode);
 	
 
