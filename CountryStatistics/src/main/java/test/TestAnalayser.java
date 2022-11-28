ff1f582c9caeb6e08d69d47aebc8af2e16b17da2
Atiqur Rahman <73983434+atiq-01@users.noreply.github.com>
2022-11-27 23:28:01 -0500

    a

--- CountryStatistics/src/main/java/test/TestAnalayser.java
+++ CountryStatistics/src/main/java/test/TestAnalayser.java
@@ -1,42 +1,35 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
 import adapters.IAdapter;
 import adapters.UnitedNationsAdapter;
-import analysers.Analyser;
+
 import analysers.AnalysisAgriVsForest;
 import analysers.IAnalyser;
+import client.UserSelection;
 
 public class TestAnalayser {
 	
 	
 	@Test 
 	public void test_analyser_01() {
-		Analyser analyser = new Analyser();
+		
+		UserSelection selection = new UserSelection();
+		selection.setCountryCode("CAN");
+		selection.setFromYear(2000);
+		selection.setToYear(2001);
+		
 		IAdapter UN = new UnitedNationsAdapter();
 		IAnalyser agrivsforest = new AnalysisAgriVsForest(UN);
-		analyser.addAnalyser(agrivsforest);
-	}
-	
-	@Test 
-	public void test_analyser_02() {
-		Analyser analyser = new Analyser();
+		
+		assertNotNull(agrivsforest.calculate(selection));
+		
 		
 	}
 	
-	@Test(expected = IllegalArgumentException.class)
-	public void test_analyser_03() {
-		Analyser analyser = new Analyser();
-		
-	}
-
-	@Test
-	public void test_analyser_04() {
-		Analyser analyser = new Analyser();
-		
-	}
 
 }
