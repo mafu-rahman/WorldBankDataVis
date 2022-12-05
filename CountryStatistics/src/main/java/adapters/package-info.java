73e3c1928fff4759241a74ca5252aa84690fc4fe
hasan-arham <77273843+hasan-arham@users.noreply.github.com>
2022-12-04 22:33:03 -0500

    added package info for adapters package

---
+++ CountryStatistics/src/main/java/adapters/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * This module contains the adapters necessary for the interaction between
+ * classes with incompatible interfaces.
+ * 
+ * <p>
+ * The Module contains the OpenCovidAdapter class used for retrieving data from
+ * the OpenCovid API and the WorldBankAdapter class used for retrieving data
+ * from the World Bank API.
+ * </p>
+ * 
+ * @since 1.2
+ * @author Arham Hasan
+ * @version 1.3
+ */
+package adapters;
