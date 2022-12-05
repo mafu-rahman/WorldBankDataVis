1b8653cd853ab88e13d2072de99a50d5b871cb3e
hasan-arham <77273843+hasan-arham@users.noreply.github.com>
2022-12-04 22:37:32 -0500

    added package info for dataFetchers package

---
+++ CountryStatistics/src/main/java/dataFetchers/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ * This module contains classes used to fetch data from the OpenCovid API and
+ * the World Bank API
+ * 
+ * @since 1.0
+ * @author Arham Hasan
+ * @version 1.1
+ */
+package dataFetchers;
