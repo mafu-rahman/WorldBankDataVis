e7bf63f510a7a23e02a68b2770206e3558086b78
hasan-arham <77273843+hasan-arham@users.noreply.github.com>
2022-12-04 22:34:47 -0500

    added package info for analysers package

---
+++ CountryStatistics/src/main/java/analysers/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * This module contains all 8 types of analysis for the application.
+ * 
+ * <p>
+ * This module contains all 8 types of analysis for the application.
+ * </p>
+ * 
+ * @since 1.0
+ * @author Arham Hasan
+ * @version 1.1
+ */
+package analysers;
