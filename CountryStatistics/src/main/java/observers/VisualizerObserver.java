9374fd2609f3a5ac1770c470d54667f4bf92cc5d
hasan-arham <77273843+hasan-arham@users.noreply.github.com>
2022-12-06 19:32:12 -0500

    changed VisualizerObserver for the observer package

--- CountryStatistics/src/main/java/observers/VisualizerObserver.java
+++ CountryStatistics/src/main/java/observers/VisualizerObserver.java
@@ -1,17 +1,22 @@
 package observers;
 
+import viewers.IViewer;
+import java.util.Vector;
+
 public class VisualizerObserver implements Observer {
 	
 	private static int tracker = 0;
 	private int observerID; 
+	private Vector<IViewer> viewers;
 	
-	public VisualizerObserver() {
+	public VisualizerObserver(Vector<IViewer> viewers) {
 		this.observerID = ++tracker;
+		this.viewers = viewers;
 		System.out.println("Observer created: " + this.observerID);
 	}
 
 	@Override
 	public void update() {
 	}
-
+	
 }
